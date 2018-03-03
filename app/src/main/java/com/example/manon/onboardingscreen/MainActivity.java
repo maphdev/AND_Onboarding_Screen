bf314c410306eb324fbe14a7ebc15fb6f61b1567
Manon Philippot <33070721+maphdev@users.noreply.github.com>
2018-03-03 20:33:19 +0100

    OnPageChangeListener and dots.

--- app/src/main/java/com/example/manon/onboardingscreen/MainActivity.java
+++ app/src/main/java/com/example/manon/onboardingscreen/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.manon.onboardingscreen;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager slideViewPager;
     private LinearLayout dotsLayout;
+
+    private TextView[] dots;
 
     private SliderAdapter sliderAdapter;
 
@@ -23,5 +27,43 @@ public class MainActivity extends AppCompatActivity {
         sliderAdapter = new SliderAdapter(this);
 
         slideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        slideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    public void addDotsIndicator(int position) {
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.colorGray));
+            dotsLayout.addView(dots[i]);
+        }
+
+        if(dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
